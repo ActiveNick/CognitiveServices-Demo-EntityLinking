cb53f9a0d1f46faf34dfb408d414aeec6e8e7541
Nick Landry <nlandry@activenick.net>
2016-08-05 10:06:55 -0400

    Added clear text feature + code comments

--- CognitiveServices-Demo-EntityLinking/MainPage.xaml.cs
+++ CognitiveServices-Demo-EntityLinking/MainPage.xaml.cs
@@ -14,12 +14,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace CognitiveServices_Demo_EntityLinking
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Demonstrate Microsoft Entity Linking Intelligence Service, part of Cognitive Services,
+    /// using a block of input text and displaying all the entities detected based on their
+    /// corresponding Wikipedia articles
     /// </summary>
     public sealed partial class MainPage : Page
     {
@@ -28,13 +28,25 @@ namespace CognitiveServices_Demo_EntityLinking
             this.InitializeComponent();
         }
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+        // Get Entitiy Linking results for input text
+        private async void btnGetResult_Click(object sender, RoutedEventArgs e)
         {
             var text = this.inputBox.Text;
+
+            // Create a new Cognitive Services Entity Linking Service client, pass our subscription ID
             var client = new EntityLinkingServiceClient("b2663927e9d242269740dd5390070e76");
+            // Send our text for entity linking
             var linkResponse = await client.LinkAsync(text);
+            // Display the results
             var result = string.Join(", ", linkResponse.Select(i => i.WikipediaID).ToList());
             this.outputBlock.Text = result;
+        }
+
+        // Clear all inputs and results
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            inputBox.Text = "";
+            outputBlock.Text = "";
         }
     }
 }
